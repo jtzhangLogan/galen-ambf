c8aadc25c81ef5117da630daa6885f620d3e3817
Tommy Liang <tommy.liang314@gmail.com>
2022-02-19 02:01:21 -0500

    added custom om message

---
+++ plugins/control_plugin/plugin_msgs/CMakeFiles/plugin_msgs_generate_messages_nodejs.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "../../../devel/share/gennodejs/ros/plugin_msgs/msg/RobotState.js"
+  "CMakeFiles/plugin_msgs_generate_messages_nodejs"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/plugin_msgs_generate_messages_nodejs.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ plugins/control_plugin/plugin_msgs/catkin_generated/pkg.develspace.context.pc.py
@@ -0,0 +1,8 @@
+# generated from catkin/cmake/template/pkg.context.pc.in
+CATKIN_PACKAGE_PREFIX = ""
+PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/galen/Galen_AMBF_Simulation/devel/include".split(';') if "/home/galen/Galen_AMBF_Simulation/devel/include" != "" else []
+PROJECT_CATKIN_DEPENDS = "message_runtime".replace(';', ' ')
+PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
+PROJECT_NAME = "plugin_msgs"
+PROJECT_SPACE_DIR = "/home/galen/Galen_AMBF_Simulation/devel"
+PROJECT_VERSION = "0.0.0"
